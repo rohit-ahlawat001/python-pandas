92fea83f2777db5d3a1cf300e5e88111611a846c
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-22 22:14:46 +0530

    print the python dict after converting into the pandas data frame

--- dataSave.py
+++ dataSave.py
@@ -1,8 +1,12 @@
 #Saving dictonary data into the .csv file
 
-studentData = {
-    "Name": ["Rohit" "Mohit", "Bharti", "Shivansh", "Shavgi"],
-     "Age": [25,30, 29, 7, 0.7],
-     "City": ["Noida", "Chandigarh", "Ahmdanagar", "Dubai", "Australia"]
+import pandas as pd
 
+studentData = {
+    "Name": ["Rohit", "Mohit", "Bharti", "Shivansh", "Shavgi"],
+     "Age": [25,30, 29, 2, 1],
+     "City": ["Noida", "Chandigarh", "Ahmdanagar", "Dubai", "Australia"]
 }
+
+df = pd.DataFrame(studentData)
+print(df)
