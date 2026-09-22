16fe110da04f00b6f4ce0bf100c6cdfcb78d39aa
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-22 19:35:43 +0530

    printing the bottom 10 results

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ df = pd.read_csv("UserData_export.csv")
 
 print("printing the Top 10 reaults from the data")
 print(df.head(10))
+
+print("Printing the bottom 10 results from the data")
 print(df.tail(10))
