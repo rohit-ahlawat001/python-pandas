5a1efde4a4b7e9070f6614470933aecb00305dbb
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-22 19:36:25 +0530

    comment out the exicting code

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pandas as pd
 df = pd.read_csv("UserData_export.csv")
 # print(df)
 
-print("printing the Top 10 reaults from the data")
-print(df.head(10))
+# print("printing the Top 10 reaults from the data")
+# print(df.head(10))
 
-print("Printing the bottom 10 results from the data")
-print(df.tail(10))
+# print("Printing the bottom 10 results from the data")
+# print(df.tail(10))
