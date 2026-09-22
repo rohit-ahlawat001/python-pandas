d27288ed59113b74c1e1e09303ead4f9b7013144
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-22 22:09:55 +0530

    create python dictonary data type

--- dataSave.py
+++ dataSave.py
@@ -1,1 +1,8 @@
 #Saving dictonary data into the .csv file
+
+studentData = {
+    "Name": ["Rohit" "Mohit", "Bharti", "Shivansh", "Shavgi"],
+     "Age": [25,30, 29, 7, 0.7],
+     "City": ["Noida", "Chandigarh", "Ahmdanagar", "Dubai", "Australia"]
+
+}
